38837821b28f10c082c905940db87ecac03aa133
Pizzu <pizzulao@gmail.com>
2025-05-15 19:20:22 -0400

    create publishJSON function and write a message to an exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -22,6 +24,20 @@ func main() {
 
 	defer conn.Close()
 	fmt.Println("Connection successfully made...")
+
+	channel, err := conn.Channel()
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	playingState := routing.PlayingState{IsPaused: true}
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, playingState)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
