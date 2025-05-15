035500971a177ef57dceb50063d822de91670fce
Pizzu <pizzulao@gmail.com>
2025-05-15 17:56:30 -0400

    create rabbitMq connection and use signals to gracefully shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const rabbitMqConnStr string = "amqp://guest:guest@localhost:15672/"
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial(rabbitMqConnStr)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	defer conn.Close()
+	fmt.Println("Connection successfully made...")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	log.Println("Program gracefully stopped")
+
 }
